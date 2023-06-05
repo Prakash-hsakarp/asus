bd7252a8b180fdd231cd11fb7d67692bbded4b39
Prakash <ramprakash.r@benchmarkbroadcast.com>
2023-06-05 15:19:33 +0800

    Add project files.

---
+++ Prakash_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Prakash_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Prakash_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("79484F8A-C495-4D04-A7E8-745BAD8DED2C")]
+[assembly: AssemblyVersion("1.0.0.0")]
